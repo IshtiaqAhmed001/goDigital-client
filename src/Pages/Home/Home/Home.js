f5e6b294aa5257742ba7b89dbae5d99988b24a86
IshtiaqAhmed001 <ishtiaq.wasi@gmail.com>
2022-02-23 02:22:24 +0600

    added top banner

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navigation from '../../Shared/Navigation/Navigation';
+import TopBanner from '../TopBanner/TopBanner';
+
+const Home = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+            <TopBanner></TopBanner>
+            <h1>sdfsdfsdf</h1>
+            <h1>sdfsdfsdf</h1>
+            <h1>sdfsdfsdf</h1>
+            <h1>sdfsdfsdf</h1>
+            <h1>sdfsdfsdf</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Pages/Home/TopBanner/TopBanner.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import './TopBanner.css';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { blueGrey } from '@mui/material/colors';
+
+
+const TopBanner = () => {
+
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: blueGrey[900],
+            }
+        }
+    });
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Box className='top-banner-heading' sx={{ flexGrow: 1 }}>
+                <Typography sx={{ fontSize: { sm: '6rem', xs: '3rem' }, textAlign: 'left', lineHeight: 1.2, fontWeight: 'bold', ml: 2, color: '#232323', paddingTop: 10, display: 'flex', alignItems: 'center' }} component="div" gutterBottom>
+                    Make Your Vision
+                    <br />
+                    A Reality
+                    <br />
+                    With Go-Digital
+                </Typography>
+                <Typography sx={{ textAlign: 'left', ml: 2 }}>
+                    <Button sx={{ marginLeft: 'auto', marginBottom: 10, fontSize: '1.2rem', backgroundColor: "#232323", paddingX: 4, borderColor: '#232323' }} size="large" variant="contained">Learn More</Button>
+                </Typography>
+            </Box>
+        </ThemeProvider>
+    );
+};
+
+export default TopBanner;
