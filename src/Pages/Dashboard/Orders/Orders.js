277d2996d216d84a92936d13f084821a198ae89e
IshtiaqAhmed001 <ishtiaq.wasi@gmail.com>
2022-03-06 01:33:54 +0600

    made dashboard table responsive

--- src/Pages/Dashboard/Orders/Orders.js
+++ src/Pages/Dashboard/Orders/Orders.js
@@ -10,7 +10,7 @@ import Paper from '@mui/material/Paper';
 import { Grid } from '@mui/material';
 
 const Orders = () => {
-    const { user, token, isLoading } = useAuth();
+    const { user, token } = useAuth();
     const [orders, setOrders] = useState([]);
 
     useEffect(() => {
@@ -28,44 +28,37 @@ const Orders = () => {
         <div>
             <h1>Orders: {orders.length}</h1>
 
-            <Grid container spacing={2}>
-                <Grid item xs={12} sm={12} md={12} lg={12}>
-
-                    <TableContainer component={Paper}>
-                        <Table aria-label="Orders table">
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell>Date Ordered</TableCell>
-                                    <TableCell align="right">Ordered Package</TableCell>
-                                    <TableCell align="right">Name</TableCell>
-                                    <TableCell align="right">Email</TableCell>
-                                    <TableCell align="right">Phone</TableCell>
-                                    <TableCell align="right">Action</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {orders.map((row) => (
-                                    <TableRow
-                                        key={row._id}
-                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                    >
-                                        <TableCell component="th" scope="row">
-                                            {row.orderDate}
-                                        </TableCell>
-                                        <TableCell align="right">{row.packageName}</TableCell>
-                                        <TableCell align="right">{row.userName}</TableCell>
-                                        <TableCell align="right">{row.email}</TableCell>
-                                        <TableCell align="right">{row.phone}</TableCell>
-                                        <TableCell align="right">action</TableCell>
-                                    </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-
-                </Grid>
-            </Grid>
-
+            <TableContainer component={Paper}>
+                <Table aria-label="Orders table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Date Ordered</TableCell>
+                            <TableCell align="right">Ordered Package</TableCell>
+                            <TableCell align="right">Name</TableCell>
+                            <TableCell align="right">Email</TableCell>
+                            <TableCell align="right">Phone</TableCell>
+                            <TableCell align="right">Action</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {orders.map((row) => (
+                            <TableRow
+                                key={row._id}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {row.orderDate}
+                                </TableCell>
+                                <TableCell align="right">{row.packageName}</TableCell>
+                                <TableCell align="right">{row.userName}</TableCell>
+                                <TableCell align="right">{row.email}</TableCell>
+                                <TableCell align="right">{row.phone}</TableCell>
+                                <TableCell align="right">action</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </div>
     );
 };
